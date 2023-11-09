a4d917dfdf94658773362c33cc72466506032cf2
zakary <123845855+zakkarry@users.noreply.github.com>
2023-11-09 16:09:43 -0600

    initial(mover): initial deluge-mover script
    See README

---
+++ README.md
@@ -0,0 +1,31 @@
+# `deluge-mover`
+
+`deluge-mover` is a Python script to pause torrents on a cache drive, run unRAID's mover script, and resume them after the move has been completed.
+
+## Introduction
+
+`deluge-mover` is very simple. You only need to edit the `deluge-mover.py` file with your JSON-RPC URL (this is your Deluge WebUI with /json at the end), your WebUI password, and unRAID's cache drive's absolute path. This is the path where the torrent data resides, not the mount for the drive itself. (e.g. `/mnt/cache/torrents/complete`)
+
+The concept for how this script operates was taken from [Bobokun](https://github.com/bobokun) and his mover script for qBittorrent.
+
+Special thanks to [TRaSH](https://github.com/TRaSH-Guides) for the motivation to do this.
+
+## Uses
+
+Using a cache drive for your downloads in unRAID normally requires you to manually pause torrents, or shut down your torrent-client container, to move the files to permanent storage on your array. This script will automatically pause torrents residing on your cache drive, run the mover script, and resume them after.
+
+This can be set on a timer so it periodically runs, keeping your cache drive ready for more.
+
+## Installation
+
+### Detailed Walkthrough
+
+You can find a detailed walkthrough at [TRaSH's Mover Script Guide](https://trash-guides.info/Downloaders/qBittorrent/Tips/How-to-run-the-unRaid-mover-for-qBittorrent/#plugins) that will be almost identical to the process you would do for this script. The only difference is that instead of `qbittorrent-api` you will install `requests`.
+
+### Brief Walkthrough (Advanced Users)
+
+- You will need the NerdTools and User Scripts plugins installed on your unRAID server.
+- Install `python-pip` `python-setuptools` and `python3` inside NerdTools
+- Run `pip3 install requests` from the terminal in unRAID and set up a User Script to run this command when the array starts **OR** set up a venv for the script with `requests` installed.
+- Edit `deluge-mover.py` with your WebUI URL, password, and preferred torrent age. (age is from time added to Deluge)
+- Create a new User Script to execute `deluge-mover.py` on your schedule.
---
+++ deluge-mover.py
@@ -0,0 +1,168 @@
+#!/usr/bin/env python3
+import asyncio
+import json
+import random
+import requests
+import time
+from enum import Enum
+from os import path, walk, system
+from urllib.parse import urlparse
+
+# this webui will need to be the JSON-RPC endpoint
+# this ends with '/json'
+deluge_webui = "http://localhost:8112/json"
+deluge_password = "deluged"
+
+# this is the absolute path to your cache drive's downloads
+cache_download_path = "/mnt/cache/torrents/completed"
+
+# the number of days to look back for relevant torrents to move
+age_days = 5
+
+
+# error codes we could potentiall receive
+class DelugeErrorCode(Enum):
+    NO_AUTH = 1
+    BAD_METHOD = 2
+    CALL_ERR = 3
+    RPC_FAIL = 4
+    BAD_JSON = 5
+
+
+class DelugeHandler:
+    def __init__(self):
+        self.deluge_cookie = None
+        self.session = requests.Session()
+
+    async def call(self, method, params, retries=1):
+
+        url = urlparse(deluge_webui).geturl()
+        headers = {"Content-Type": "application/json"}
+        id = random.randint(0, 0x7fffffff)
+
+        # set our cookie if we have it
+        if self.deluge_cookie:
+            headers["Cookie"] = self.deluge_cookie
+
+        if method == "auth.login":
+            print(f"Connecting to Deluge: {url}")
+
+        # send our request to the JSON-RPC endpoint
+        try:
+            response = self.session.post(
+                url,
+                data=json.dumps(
+                    {"method": method, "params": params, "id": id}),
+                headers=headers,
+            )
+            response.raise_for_status()
+        except requests.exceptions.RequestException as network_error:
+            raise ConnectionError(
+                f"Failed to connect to Deluge at {url}"
+            ) from network_error
+
+        # make sure the json response is valid
+        try:
+            json_response = response.json()
+        except json.JSONDecodeError as json_parse_error:
+            raise ValueError(
+                f"Deluge method {method} response was non-JSON: {json_parse_error}"
+            )
+        
+        # check for authorization failures, and retry once
+        if (json_response.get("error", [None]) != None):
+            if json_response.get("error", [None]).get("code") == DelugeErrorCode.NO_AUTH and retries > 0:
+                
+                self.deluge_cookie = None
+                await self.call("auth.login", [deluge_password], 0)
+
+                if self.deluge_cookie:
+                    return await self.call(method, params)
+                else:
+                    raise ConnectionError(
+                        "Connection lost with Deluge. Reauthentication failed."
+                    )
+
+        self.handle_cookies(response.headers)
+        return json_response
+
+    def handle_cookies(self, headers):
+        deluge_cookie = headers.get("Set-Cookie")
+        if deluge_cookie:
+            deluge_cookie = deluge_cookie.split(";")[0]
+        else:
+            deluge_cookie = None
+
+
+def find_file_on_cache(dir, file):
+    for root, dirs, files in walk(dir):
+        if file in files:
+            return path.join(root, file)
+    return None
+
+
+def filter_added_time(t_object, age_days):
+    cached_file = False
+    time_elapsed = int(time.time()) - t_object.get('time_added', [None])
+    current_path = path.join(cache_download_path, t_object.get('name', [None]))
+    if time_elapsed >= (age_days * 60 * 60 * 24):
+        if path.exists(current_path):
+            cached_file = True
+        elif (find_file_on_cache(cache_download_path, t_object.get('name', [None])) != None):
+            cached_file = True
+        return cached_file
+    return False
+
+
+async def main():
+    deluge_handler = DelugeHandler()
+
+    try:
+        # auth.login
+        auth_response = await deluge_handler.call("auth.login", [deluge_password], 0)
+        print("Authentication response:", auth_response)
+
+        # get torrent list
+        torrent_list = (await deluge_handler.call("web.update_ui", [["name", "save_path", "progress", "time_added"], {}])).get(
+            'result', [None]).get('torrents', [None])
+
+        # make sure list exists
+        if torrent_list != None:
+            filtered_torrents = filter(
+                lambda kv: filter_added_time(kv[1], age_days),
+                torrent_list.items()
+            )
+
+            # loop through items in torrent list
+            for hash, values in filtered_torrents:
+                print(
+                    f"{values.get('name', [None])} ({hash})"
+                    f"\n\tsave_path: {path.join(cache_download_path,
+                                         values.get('name', [None]))}\n")
+                
+                # pause relevant torrents
+                await deluge_handler.call("core.pause_torrent", [hash])
+            
+            time.sleep(10)
+
+            # run the mover
+            print("Starting Mover")
+            system("/usr/local/sbin/mover start")
+            
+            time.sleep(10)
+
+            # resume all the torrents we previously paused
+            for hash in filtered_torrents:
+                await deluge_handler.call("core.resume_torrent", [hash])
+
+    except Exception as e:
+        print(f"Error: {e}")
+
+    deluge_handler.session.close()
+
+
+async def run_main():
+    await main()
+
+if __name__ == "__main__":
+    asyncio.run(run_main())
